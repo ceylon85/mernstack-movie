f362d8796defaedd2b31be5054a08c424897d493
Heaven Voice <heavenvoice@Heavenui-MacBook-Pro.local>
2020-03-27 21:38:21 +0900

    landingpage

--- client/src/components/Config.js
+++ client/src/components/Config.js
@@ -1,9 +1,8 @@
 //SERVER ROUTES
 export const USER_SERVER = '/api/users';
 
-
 export const API_URL = "https://api.themoviedb.org/3/";
 
 export const API_KEY = "ae5cadf3939863eb04cb5826443a89b2";
 
-export const IMAGE_BASE_URL = "http://image.tmdb.org/t/p/";
+export const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/";
