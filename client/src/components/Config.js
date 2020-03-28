41e22fb909a2280b713777dbb81ce7850c7eee51
Heaven Voice <heavenvoice@Heavenui-MacBook-Pro.local>
2020-03-28 20:20:37 +0900

    landing

--- client/src/components/Config.js
+++ client/src/components/Config.js
@@ -5,4 +5,11 @@ export const API_URL = "https://api.themoviedb.org/3/";
 
 export const API_KEY = "ae5cadf3939863eb04cb5826443a89b2";
 
-export const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/";
+export const IMAGE_BASE_URL = "http://image.tmdb.org/t/p/";
+
+//Sizes: w300, w780, w1280, original
+export const BACKDROP_SIZE = 'w1280'
+export const IMAGE_SIZE = 'w1280'
+
+// w92, w154, w185, w342, w500, w780, original
+export const POSTER_SIZE = 'w500'
